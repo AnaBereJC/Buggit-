ed2bcb9d4b3d34f98d73ecf6452f98d5a573f3ae
Jhoana <jhoanavc20@gmail.com>
2024-06-11 22:49:32 -0600

    mi primer commit

---
+++ ejercicio1.py
@@ -0,0 +1,6 @@
+def suma(a, b): 
+    return a + b
+num1=float(input("introduce un numero:"))
+num2= float(input("introduce el segundo numero: "))
+resultado= suma(num1, num2)
+print(f"la suma de {num1} y {num2} es {resultado}")
